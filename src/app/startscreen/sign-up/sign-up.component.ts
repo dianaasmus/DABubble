edaf3a988ce27364314636ee4294f8cdb18b05e0
Diana Asmus <asmus.diana@icloud.com>
2023-12-22 23:57:39 +0100

    Startscreen animation + signUp FormControl

--- src/app/startscreen/sign-up/sign-up.component.ts
+++ src/app/startscreen/sign-up/sign-up.component.ts
@@ -1,29 +1,51 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LegalNoticeComponent } from '../legal-notice/legal-notice.component';
 import { StartscreenComponent } from '../startscreen.component';
+import { NgIf } from '@angular/common';
 
 
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [ReactiveFormsModule, LegalNoticeComponent],
+  imports: [ReactiveFormsModule, LegalNoticeComponent, NgIf],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
-  newUser = new FormGroup({
-    firstLastName: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
+  checkData = false;
+
+
+  // newUser = new FormGroup({
+  //   firstLastName: new FormControl(''),
+  //   email: new FormControl(''),
+  //   password: new FormControl(''),
+  //   checkData: new FormControl(this.checkData)
+  // });
+
+  newUser = this.fb.group({
+    firstLastName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+\s[a-zA-Z]+$/)]],
+    email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
+    password: ['', [Validators.required, Validators.pattern(/^(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{8,}$/)]],
+    checkData: [this.checkData, Validators.requiredTrue]
   });
+
+
+  constructor(public startscreen: StartscreenComponent, private fb: FormBuilder) {}
+
+
+  onCheckboxChange(event: Event) {
+    this.checkData = (event.target as HTMLInputElement).checked;
+  }
+
 
   submitForm(e: any) {
     e.preventDefault();
+    console.log(this.newUser);
+    
   }
 
-  constructor(public startscreen: StartscreenComponent) {}
 
   toggleSignup() {
     this.startscreen.toggleSignup();
